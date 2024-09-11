b6033815428623c9965ad10b6bd682fcc18bfee5
Mathieu GRUSON <mathieu.gruson@gmail.com>
2024-09-11 17:10:50 +0200

    ft_strlen improve

--- ft_strlen.s
+++ ft_strlen.s
@@ -1,5 +1,5 @@
 section .data
-    text db "Hello, world!", 0    ; A null-terminated string
+    text db "He", 0    ; A null-terminated string
 
 section .text
 global _start
@@ -12,20 +12,16 @@ _start:
     call ft_strlen
 
     ; Store the result in EAX (number of characters) for exit code
-    mov eax, 1               ; syscall number for exit
-    mov edi, eax             ; exit code (number of characters returned from ft_strlen)
+    mov eax, 60               ; syscall number for exit
     syscall                  ; make the system call to exit
 
 ft_strlen:
-    xor rcx, rcx             ; Clear counter register (RCX)
 
 .count_loop:
     cmp byte [rdi], 0        ; Compare the byte at RDI with 0 (null terminator)
     je .done                 ; If zero, jump to the end
     inc rdi                  ; Move to the next character
-    inc rcx                  ; Increment the counter
     jmp .count_loop          ; Repeat the loop
 
 .done:
-    mov rax, rcx             ; Move the counter value to RAX (return value)
     ret                      ; Return
