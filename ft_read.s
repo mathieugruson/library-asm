e516adf399c4836b7b4461abd5abc7c9092dc01b
Mathieu GRUSON <mathieu.gruson@gmail.com>
2024-09-14 10:34:20 +0200

    improve read and write

--- ft_read.s
+++ ft_read.s
@@ -11,16 +11,17 @@ section .text
         syscall
 
         cmp rax, 0          ; check for error
-        jl .errno           ; Check if there was an error (set sign flag)
-        ret
+        jns .done
 
-	.errno:
-		mov rdi, rax                    ; rdi = error code
+        push rax
         call __errno_location wrt ..plt ; Call __errno_location to get the address of errno (wrt ..plt to handle a error msg when compiling)
 
-		neg rdi         ; sys_write return the error with a negative sign as opposite to the one in errno.h because positive number are reserved to the number of char printed 
+		pop rdi
+        neg rdi         ; sys_write return the error with a negative sign as opposite to the one in errno.h because positive number are reserved to the number of char printed 
 		mov [rax], rdi  ; call __errno_location return the adress of errno in rax so we need to put the error in the adress for errno to access it
 
 		mov rax, -1     ; return -1 ; put -1 as in write function as rax is the return value
 		ret
 
+        .done:
+        ret
--- ft_write.s
+++ ft_write.s
@@ -7,22 +7,23 @@ section .text
     ft_write:
 
         mov rax, 1          ; syscall number for write
-        syscall ; not necessary to pass the arg. It is done automatically sometimes
+        syscall ; not necessary to pass the arg rdi and rsi because they are already there. It is done automatically sometimes
 
 		cmp rax, 0 ; check for error
-        jl .errno           ; Check if there was an error (set sign flag)
-        ret
+        jns .done
 
-	.errno:
-		mov rdi, rax ; rdi = error code
+		push rax
         call __errno_location wrt ..plt ; Call __errno_location to get the address of errno (wrt ..plt to handle a error msg when compiling)
 
-		neg rdi ; sys_write return the error with a negative sign as opposite to the one in errno.h because positive number are reserved to the number of char printed 
-		mov [rax], rdi ; call __errno_location return the adress of errno in rax so we need to put the error in the adress for errno to access it
-
-		mov rax, -1 ; return -1 ; put -1 as in write function as rax is the return value
+        pop rdi                           ; rdi = error code
+        neg rdi                           ; Make error code positive
+        mov [rax], rdi                    ; Store the error in errno
+		
+        mov rax, -1 ; return -1 ; put -1 as in write function as rax is the return value
 		ret
-
+        
+        .done:
+            ret
 
 ; https://android.googlesource.com/kernel/lk/+/dima/for-travis/include/errno.h
 ; https://www.tortall.net/projects/yasm/manual/html/objfmt-elf64.html
